c99e8928da7ca7b7785e224fb314ff91721f88ed
bogusz <bogusz2005@gmail.com>
2024-02-08 23:00:59 +0100

    ptr_provider.cpp

--- src/bsdb/ptr_provider.cpp
+++ src/bsdb/ptr_provider.cpp
@@ -4,5 +4,21 @@
 
 #include "ptr_provider.h"
 
+#include <iostream>
+
 namespace bsdb {
+    ptr_provider::ptr_provider(std::vector<unsigned long> *ptrs,const unsigned long &ptr): ptrs(ptrs) {
+        ptrs->push_back(ptr);
+        it=--ptrs->end();
+        std::cout << "con ptr_provider: ptr=" <<  ptr << " it=" << *it << "\n";
+    }
+
+    ptr_provider::~ptr_provider() {
+        ptrs->erase(it);
+    }
+
+    unsigned long ptr_provider::get() const {
+        return *it;
+    }
+
 } // bsdb
